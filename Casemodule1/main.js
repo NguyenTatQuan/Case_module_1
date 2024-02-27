960bc482d770d38fa60237fb353eff701c862557
kikon123 <tatquan2001@gmail.com>
2024-02-27 21:27:25 +0700

    abv

--- Casemodule1/main.js
+++ Casemodule1/main.js
@@ -1,37 +1,62 @@
-let canvas = document.getElementById("gamazone");
-let context = document.getElementById("2d");
-let  scoreshow = document.getElementById("score");
+let student = new Student('01', "Nguyễn Tất Quân", '30/11/2001', 'Nam','UDTS24.01','https://i.pinimg.com/564x/6f/3b/8f/6f3b8f2937e5c326da3f327112cf819f.jpg' );
+let student1 = new Student('02','Phan Thanh Thảo','01/08/1999','Nữ', 'UDTS24.02','https://i.pinimg.com/564x/f1/43/64/f1436415a2a208043bdef80c73d66b4a.jpg');
+let student2 = new Student('03','Trần Ngọc Duy','18/09/1995','Nam','UDTS24.03','https://i.pinimg.com/564x/bf/84/39/bf8439fe825fff289c6472d34b3b7f77.jpg');
+let arr =[student, student1, student2];
+let manage = new StudentManagement(arr);
 
-let birding = new  Image();
-let hinhnenchinh = new  Image();
-let ongtren = new  Image();
-let ongduoi = new  Image();
-birding.src="images/bird.png"
-hinhnenchinh.src="images/hinhnenchinh.png"
-ongtren.src="images/ongtren.png"
-ongduoi.src="images/ongduoi.png"
+function addStudent(){
+    let id = document.getElementById('id').value;
+    let name = document.getElementById('fullname').value;
+    let date = document.getElementById('date').value;
+    let gender = document.getElementById('gender').value;
+    let grade = document.getElementById('grade').value;
+    let img = document.getElementById('img').value;
 
-let score=0;
-let khoangcachhaiong=150
-let khoangcachdenongduoi;
-
-let bird={
-    x:canvas.width,
-    y:0
+    let student = new Student(id,name,date,gender,grade,img);
+    manage.addStudent(student);
+    manage.showList();
+    clear();
 }
 
-function run(){
-    context.drawImage(hinhnenchinh,0,0);
-    context.drawImage(birding,bird,x,bird,y)
-
-    for (let i=0;i<ong.length;i++){
-        khoangcachdenongduoi=ongtren.height+khoangcachhaiong
-
-    }
-    bird.y+=3;
-    requestAnimationFrame(run);
+function clear(){
+    document.getElementById('id').value = '';
+    document.getElementById('fullname').value = '';
+    document.getElementById('date').value = '';
+    document.getElementById('gender').value = '';
+    document.getElementById('grade').value = '';
+    document.getElementById('img').value= '';
 }
-document.addEventListener("keydown",function (){
-    bird.y-=60
-})
-run()
+
+function deleteStudent(id) {
+    manage.delete(id);
+    manage.showList();
+}
+
+let studentId = 0;
+function editStudent(id){
+    let student = manage.findStudentById(id);
+    document.getElementById('id').value = student.id;
+    document.getElementById('fullname').value = student.fullname;
+    document.getElementById('date').value = student.date;
+    document.getElementById('gender').value = student.gender;
+    document.getElementById('grade').value = student.grade;
+    document.getElementById('img').value = student.img;
+
+    studentId = id;
+}
+
+function updateStudent(){
+    let id = document.getElementById('id').value;
+    let name = document.getElementById('fullname').value;
+    let date = document.getElementById('date').value;
+    let gender = document.getElementById('gender').value;
+    let grade = document.getElementById('grade').value;
+    let img = document.getElementById('img').value;
+
+    let student = manage.findStudentById(studentId);
+    manage.edit(student,id,name,date,gender,grade, img);
+    manage.showList();
+    clear();
+}
+
+manage.showList();
